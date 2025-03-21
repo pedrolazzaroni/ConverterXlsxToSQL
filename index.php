8e8724af13735de5df52240e66cb4e3bb78bf007
pedrolazzaroni <phlazzaroni@gmail.com>
2025-03-21 13:26:28 -0300

    Ajustando titulo

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $error = isset($_GET['error']) ? $_GET['error'] : null;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Excel para SQL | Conversor Online</title>
+    <title>ExcelToSQL | Conversor Online</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
